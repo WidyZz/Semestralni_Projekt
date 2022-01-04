ba40a03db6b288a2120c28738b0bee63d7bbdfae
Widy <vitchvoj@gmail.com>
2022-01-04 23:20:08 +0100

    Update query prikazu

--- ChvojProjekt/Core/DBDataGrid.cs
+++ ChvojProjekt/Core/DBDataGrid.cs
@@ -15,8 +15,8 @@ namespace ChvojProjekt.Core
         //Prop
         private string queryObjednavky = "SELECT o.Id as 'Číslo objednávky', a.Jmeno as 'Zákazník', p.Nazev as 'Název Produktu', k.Celkem_Kusu as 'Počet kusů', k.Celkem_Kusu * p.Cena as 'Cena' from Objednavky o inner join Produkt p on ProduktID=p.Id inner join Kosik k on KosikID=k.Id inner join Auth a on o.ZakaznikID=a.Id;";
         private string queryUzivatele = "select ID, Jmeno, email as 'E-mail', Telefon, isadmin as 'Admin?' from Auth order by IsAdmin";
-        private string queryProdukt = "select * from Produkt order by Kusu DESC";
-        private string queryKosik = "select * from Kosik order by Vytvoreno DESC";
+        private string queryProdukt = "select Id as 'Číslo produktu', Nazev as 'Název produktu', Hmotnost, Kusu as 'Kusů skladem', Cena from Produkt where Kusu > 0 order by Kusu DESC";
+        private string queryKosik = "Select Id as 'Číslo košíku', ZakaznikID as 'ID Zákazníka', ProduktID as 'Číslo produktu', Celkem_Kusu as 'Počet kusů', Vytvoreno as 'Vytvořeno' From Kosik order by Vytvoreno DESC";
 
         private SqlConnection _connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Users\vitch\Desktop\ChvojProjekt\ChvojProjekt\DB\DB.mdf;Integrated Security=True");
         // Vypis objednavek z dbo.Objednavky
