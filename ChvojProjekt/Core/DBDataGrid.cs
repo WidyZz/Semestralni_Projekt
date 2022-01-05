4fac36669448eb59804c3fdb8b76c8d2f45377b8
Widy <vitchvoj@gmail.com>
2022-01-05 18:13:20 +0100

    Refactoring

--- ChvojProjekt/Core/DBDataGrid.cs
+++ ChvojProjekt/Core/DBDataGrid.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,10 +14,11 @@ namespace ChvojProjekt.Core
     public class DBDataGrid
     {
         //Prop
-        private string queryObjednavky = "SELECT o.Id as 'Číslo objednávky', a.Jmeno as 'Zákazník', p.Nazev as 'Název Produktu', k.Celkem_Kusu as 'Počet kusů', k.Celkem_Kusu * p.Cena as 'Cena' from Objednavky o inner join Produkt p on ProduktID=p.Id inner join Kosik k on KosikID=k.Id inner join Auth a on o.ZakaznikID=a.Id;";
-        private string queryUzivatele = "select ID, Jmeno, email as 'E-mail', Telefon, isadmin as 'Admin?' from Auth order by IsAdmin";
-        private string queryProdukt = "select Id as 'Číslo produktu', Nazev as 'Název produktu', Hmotnost, Kusu as 'Kusů skladem', Cena from Produkt where Kusu > 0 order by Kusu DESC";
-        private string queryKosik = "Select Id as 'Číslo košíku', ZakaznikID as 'ID Zákazníka', ProduktID as 'Číslo produktu', Celkem_Kusu as 'Počet kusů', Vytvoreno as 'Vytvořeno' From Kosik order by Vytvoreno DESC";
+        private string queryObjednavky = File.ReadAllText(@"D:\Users\vitch\Desktop\ChvojProjekt\ObjednavkyQuery.sql");
+        private string queryUzivatele = File.ReadAllText(@"D:\Users\vitch\Desktop\ChvojProjekt\AuthQuery.sql");
+        private string queryProdukt = File.ReadAllText(@"D:\Users\vitch\Desktop\ChvojProjekt\ProduktyQuery.sql");
+        private string queryKosik = File.ReadAllText(@"D:\Users\vitch\Desktop\ChvojProjekt\KosikQuery.sql");
+        private string queryObjednat = File.ReadAllText(@"D:\Users\vitch\Desktop\ChvojProjekt\Objednat.sql");
 
         private SqlConnection _connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Users\vitch\Desktop\ChvojProjekt\ChvojProjekt\DB\DB.mdf;Integrated Security=True");
         // Vypis objednavek z dbo.Objednavky
@@ -40,7 +42,6 @@ namespace ChvojProjekt.Core
         // Vypis objednavek z dbo.Kosik
         public void SQLKosik(DataTable dtbl)
         {
-            SqlDataAdapter adapter = new SqlDataAdapter();
             SqlDataAdapter sda = new SqlDataAdapter(queryKosik, _connection);
             sda.Fill(dtbl);
         }
