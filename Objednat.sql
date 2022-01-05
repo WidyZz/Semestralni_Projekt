12fd2e2fadd2011a31debb50cb03ececdc4dbb81
Widy <vitchvoj@gmail.com>
2022-01-05 18:11:44 +0100

    Refactoring

---
+++ Objednat.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO Objednavky (ZakaznikID, KosikID, ProduktID, Cena, CasObjednani)
+SELECT k.ZakaznikID, k.Id, k.ProduktID, k.Celkem_Kusu * p.Cena, GETDATE() from Kosik k inner join Produkt p
+ON k.ProduktID = p.Id
+WHERE k.ZakaznikID = 1;
+DELETE FROM Kosik where ZakaznikID = 1;
