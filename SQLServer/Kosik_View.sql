3cadc6a913a5810abeb90c386be577dd9196bf11
Widy <60298890+WidyZz@users.noreply.github.com>
2022-01-09 11:20:03 +0100

    Delete SQLServer directory

--- SQLServer/Kosik_View.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE VIEW [dbo].[Kosik_View]
-	AS
-	SELECT [p].Id AS IdProduktu, [p].Nazev as [Nazev Produktu]
-	FROM dbo.Produkt p
--- SQLServer/ZakaznikT.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Zakaznik]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Jmeno] NCHAR(10) NULL, 
-    [Prijmeni] NCHAR(10) NULL, 
-    [Email] NCHAR(40) NULL, 
-    [Datum Narozeni] DATE NULL
-)
